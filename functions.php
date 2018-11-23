493eb646db79afa06f416a435b5f0ba3616d8b83
MitchElbourne <mkelbourne@gmail.com>
2018-11-23 09:27:43 +0000

    Initial site commit with minimal work on nav

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function my_theme_enqueue_styles () {
+  // WP enqueue styles
+  wp_enqueue_style('nunitofont', get_stylesheet_directory_uri('https://fonts.googleapis.com/css?family=Nunito:400,700'));
+  wp_enqueue_style('montserratfont', get_stylesheet_directory_uri('https://fonts.googleapis.com/css?family=Montserrat:400,700'));
+  wp_enqueue_style('bootstrap4', get_stylesheet_directory_uri('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+  wp_enqueue_script('jquery', "https://code.jquery.com/jquery-3.3.1.slim.min.js", array('jquery'), false, true);
+  wp_enqueue_script('popper', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js", array('jquery'), false, true);
+  wp_enqueue_script('bootstrapjs', "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js", array('jquery'), false, true);
+  wp_enqueue_script('tether', "https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js", array('jquery'), false, true);
+
+  // Enqueue custom styles
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+  // TODO: Add JavaScript
+  // Register custom scripts
+  // wp_enqueue_script('main_script', get_theme_file_uri('assets/js/scripts.js'), array('jquery'), microtime(), true);
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+wp_enqueue_template(get_template_directory_uri() . 'wp_bootstrap_navlist_walker.php');
+
+?>
---
+++ header.php
@@ -0,0 +1,50 @@
+<?php
+// The Header for the One-Page-Child Theme
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php if (is_singular() && pings_open(get_queried_object())) : ?>
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php endif; ?>
+    <?php wp_head(); ?>
+  </head>
+  <body id="page-top" <?php body_class('index'); ?>>
+    <!-- Header Content -->
+    <div class="header">
+      <div class="header-container">
+        <div class="clearfix top"></div>
+
+
+
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-light navbar-expand-sm fixed-top" id="mainNav">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false" aria-controls="navbarResponsive" aria-label="Toggle Navigation">
+                <span>More</span>
+                <i class="fas fa-bars"></i>
+              </button>
+              <a class="navbar-brand" href="#"><img src="<?php echo get_theme_file_uri('/assets/images/codeclub-iom-logo.svg'); ?>"/></a>
+            </div>
+            <!-- Navigation Links and Box -->
+              <?php
+          		wp_nav_menu(array(
+          			'menu'              => 'Menu 1',
+          			'depth'             => 1,
+          			'container'         => 'div',
+          			'container_class'   => 'collapse navbar-collapse',
+          			'container_id'      => 'navbarResponsive',
+          			'menu_class'        => 'nav navbar-nav',
+          		));
+          		?>
+            </div><!-- /.container-fluid -->
+          </nav>
+        </div>
+      </div>
+      <!-- /Header Content -->
