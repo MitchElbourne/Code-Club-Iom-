d95f69a731d6c5520682de1116f94f678a91cc8a
Mitchell Elbourne <mkelbourne@gmail.com>
2018-12-12 11:02:06 +0000

    Finished new styling for events on home-page

--- index.php
+++ index.php
@@ -27,126 +27,144 @@ get_header();
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-8 news">
-<?php
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-  $recentPosts = new WP_Query(array(
-    'post_type' => 'post',
-    'posts_per_page' => 9,
-    'post_order' => "ASC",
-    'paged' => $paged,
-  ));
+      <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $recentPosts = new WP_Query(array(
+          'post_type' => 'post',
+          'posts_per_page' => 9,
+          'post_order' => "ASC",
+          'paged' => $paged,
+        ));
 
-  // Variable used for retreiving the first post from the array
-  $postcount = 0;
-  while ($recentPosts->have_posts()) {
-    $recentPosts->the_post();
-    $date = date('d.m.y', strtotime(get_the_date()));
-    $image;
-    if (!get_the_post_thumbnail_url()) {
-      $image = get_theme_file_uri('/assets/images/2018-Digital-Marketing.jpg');
-    } else {
-      $image = get_the_post_thumbnail_url();
-    }
+        // Variable used for retreiving the first post from the array
+        $postcount = 0;
+        while ($recentPosts->have_posts()) {
+          $recentPosts->the_post();
+          $date = date('d.m.y', strtotime(get_the_date()));
+          $image;
+          if (!get_the_post_thumbnail_url()) {
+            $image = get_theme_file_uri('/assets/images/2018-Digital-Marketing.jpg');
+          } else {
+            $image = get_the_post_thumbnail_url();
+          }
 
-    if ($postcount == 0) { ?>
-      <article class="post-item first-post">
-        <div class="post-thumbnail">
-          <div class="ratio-container">
-            <div class="ratio-content" style="background-image: url('<?php echo $image; ?>')"></div>
-          </div>
-          <a href="<?php the_permalink(); ?>" class="post-overlay"></a>
-        </div><!-- post-thumbnail -->
-        <div class="post-categories">
-          <?php
-          the_category();
-          ?>
-        </div><!-- post-categories -->
-        <a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a><!-- post-title -->
-        <div class="post-content">
-          <p><?php echo wp_trim_words(get_the_content(), 55); ?></p>
-        </div><!-- post-content -->
+          if ($postcount == 0) { ?>
+            <article class="post-item first-post">
+              <div class="post-thumbnail">
+                <div class="ratio-container">
+                  <div class="ratio-content" style="background-image: url('<?php echo $image; ?>')"></div>
+                </div>
+                <a href="<?php the_permalink(); ?>" class="post-overlay"></a>
+              </div><!-- post-thumbnail -->
+              <div class="post-categories">
+                <?php
+                the_category();
+                ?>
+              </div><!-- post-categories -->
+              <a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a><!-- post-title -->
+              <div class="post-content">
+                <p><?php echo wp_trim_words(get_the_content(), 55); ?></p>
+              </div><!-- post-content -->
 
-        <div class="post-meta">
-          <a href="<?php echo the_permalink(); ?>" class="cta cta-read-more">Read more</a>
-          <div class="post-meta-content pull-right">
-            <a href="<?php echo get_author_posts_url($post->post_author); ?>" class="post-author"><img src="<?php echo get_theme_file_uri('/assets/images/Icons/icon-pen.svg'); ?>" alt="icon"><?php the_author(); ?></a>
-            <a class="post-date"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-calendar.svg'); ?>" alt="icon"><?php echo $date; ?></a>
-            <a href="<?php echo the_permalink(); ?>" class="post-comments has-comment"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-comment0.svg'); ?>" alt="icon"><?php echo get_comments_number(); ?></a>
-          </div>
-        </div><!-- post-meta -->
-      </article><!-- article -->
-      <div class="row blog-articles">
-    <?php  $postcount++;
-    } else { ?>
-
-      <!-- Individual Blog Posts -->
-      <div class="col-md-6">
-        <article class="post-item">
-          <div class="post-thumbnail">
-            <div class="ratio-container">
-              <div class="ratio-content" style="background-image: url('<?php echo $image; ?>');"></div>
-            </div>
-            <a href="<?php echo get_the_permalink(); ?>" class="post-overlay"></a>
-          </div><!-- post-thumbnail -->
-          <div class="post-categories">
-            <?php
-            the_category();
-            ?>
-          </div><!-- post-categories -->
-          <a href="<?php echo get_the_permalink(); ?>" class="post-title"><?php the_title(); ?></a><!-- post-title -->
-          <div class="post-meta">
-            <div class="post-meta-content">
-              <a href="<?php echo get_author_posts_url($post->post_author); ?>" class="post-author"><img src="<?php echo get_theme_file_uri('/assets/images/Icons/icon-pen.svg'); ?>" alt="icon"><?php the_author(); ?></a>
-              <a class="post-date"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-calendar.svg'); ?>" alt="icon"><?php echo $date; ?></a>
-              <a href="<?php echo the_permalink(); ?>" class="post-comments has-comment"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-comment0.svg'); ?>" alt="icon"><?php echo get_comments_number(); ?></a>
-            </div>
-          </div><!-- post-meta -->
-          <div class="post-content">
-            <p><?php echo wp_trim_words(get_the_content(), 35); ?></p>
-          </div><!-- post-content -->
-        </article><!-- article -->
-      </div>
-    <?php }
-    wp_reset_query();
-  } ?>
-
-      <div class="pagination">
-        <div class="prev"><?php previous_posts_link('Previous posts', $postslist->max_num_pages); ?></div>
-        <div class="next"><?php next_posts_link('Next posts', $postslist->max_num_pages); ?></div>
-      </div>
-
-
-        </div><!-- row -->
-      </div><!-- row -->
-      <div class="col-12 col-sm-12 col-md-12 col-lg-4">
-        <?php dynamic_sidebar('primary-widget-area'); ?>
-        <div class="upcoming-events">
-          <h3>Upcoming Events!</h3>
-          <?php
-          $events = new WP_Query(array(
-            'post_type' => 'event',
-            'order' => 'ASC',
-          ));
-          if ($events) {
-            while ($events->have_posts()) {
-              $events->the_post();
-              $eventdate = date('d.m.y', strtotime(get_field('event_date')));
-              ?>
-
-              <div class="event">
-                <a href="<?php the_permalink(); ?>">
-                  <h4 class="event-title"><?php the_title(); ?></h4>
-                  <div class="event-datetime">
-                    <p class="event-date"><?php echo $eventdate ?></p>
-                    <span class="break"></span>
-                    <p class="event-time"><?php the_field('event_time_start'); ?> - <?php the_field('event_time_end'); ?></p>
-                  </div>
-                  <p class="event-content"><?php echo wp_trim_words(get_field('event_description'), 20); ?></p>
-                  <a class="cta-read-more" href="<?php the_permalink(); ?>">Read More</a>
-                </a>
-              </div>
-            <?php }
+              <div class="post-meta">
+                <a href="<?php echo the_permalink(); ?>" class="cta cta-read-more">Read more</a>
+                <div class="post-meta-content pull-right">
+                  <a href="<?php echo get_author_posts_url($post->post_author); ?>" class="post-author"><img src="<?php echo get_theme_file_uri('/assets/images/Icons/icon-pen.svg'); ?>" alt="icon"><?php the_author(); ?></a>
+                  <a class="post-date"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-calendar.svg'); ?>" alt="icon"><?php echo $date; ?></a>
+                  <a href="<?php echo the_permalink(); ?>" class="post-comments has-comment"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-comment0.svg'); ?>" alt="icon"><?php echo get_comments_number(); ?></a>
+                </div>
+              </div><!-- post-meta -->
+            </article><!-- article -->
+            <div class="row blog-articles">
+          <?php  $postcount++;
           } else { ?>
+
+            <!-- Individual Blog Posts -->
+            <div class="col-md-6">
+              <article class="post-item">
+                <div class="post-thumbnail">
+                  <div class="ratio-container">
+                    <div class="ratio-content" style="background-image: url('<?php echo $image; ?>');"></div>
+                  </div>
+                  <a href="<?php echo get_the_permalink(); ?>" class="post-overlay"></a>
+                </div><!-- post-thumbnail -->
+                <div class="post-categories">
+                  <?php
+                  the_category();
+                  ?>
+                </div><!-- post-categories -->
+                <a href="<?php echo get_the_permalink(); ?>" class="post-title"><?php the_title(); ?></a><!-- post-title -->
+                <div class="post-meta">
+                  <div class="post-meta-content">
+                    <a href="<?php echo get_author_posts_url($post->post_author); ?>" class="post-author"><img src="<?php echo get_theme_file_uri('/assets/images/Icons/icon-pen.svg'); ?>" alt="icon"><?php the_author(); ?></a>
+                    <a class="post-date"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-calendar.svg'); ?>" alt="icon"><?php echo $date; ?></a>
+                    <a href="<?php echo the_permalink(); ?>" class="post-comments has-comment"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-comment0.svg'); ?>" alt="icon"><?php echo get_comments_number(); ?></a>
+                  </div>
+                </div><!-- post-meta -->
+                <div class="post-content">
+                  <p><?php echo wp_trim_words(get_the_content(), 35); ?></p>
+                </div><!-- post-content -->
+              </article><!-- article -->
+            </div>
+          <?php }
+          wp_reset_query();
+        } ?>
+
+        <div class="pagination">
+          <div class="prev"><?php previous_posts_link('Previous posts', $postslist->max_num_pages); ?></div>
+          <div class="next"><?php next_posts_link('Next posts', $postslist->max_num_pages); ?></div>
+        </div>
+
+
+          </div><!-- row -->
+        </div><!-- row -->
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php dynamic_sidebar('primary-widget-area'); ?>
+          <div class="upcoming-events">
+            <h3>Upcoming Events</h3>
+
+
+            <?php
+            // Php WP Query for pulling in the 4 most recent event dates
+            $today = date('Ymd');
+            $events = new WP_Query(array(
+              'post_type' => 'event',
+              'posts_per_page' => 4,
+              'order' => 'ASC',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_query' => array(
+                array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numberic'
+                )
+              ),
+            ));
+
+
+            if ($events) {
+              while ($events->have_posts()) {
+                $events->the_post();
+                $eventdate = date('M, Y', strtotime(get_field('event_date')));
+                ?>
+
+                <div class="event">
+                  <a href="<?php the_permalink(); ?>">
+                    <h4 class="event-title"><?php the_title(); ?></h4>
+                    <div class="event-datetime">
+                      <p class="event-date"><?php echo $eventdate ?></p>
+                      <span class="break"></span>
+                      <p class="event-time"><?php the_field('event_time_start'); ?> to <?php the_field('event_time_end'); ?></p>
+                    </div>
+                    <p class="event-content"><?php echo wp_trim_words(get_field('event_description'), 20); ?></p>
+                    <button class="cta-read-more" href="<?php the_permalink(); ?>">Book your spot</button>
+                  </a>
+                </div>
+              <?php }
+            } else { ?>
             <p class="no-event-listings">
               Sorry, there don't seem to be any upcoming events. Keep your eye on this page though, there could be one soon!
             </p>
---
+++ search.php
@@ -0,0 +1,97 @@
+<?php
+get_header();
+?>
+<!-- blog title -->
+
+<!-- <h1 class="index_titles blog single"><?php printf( __( 'Search Results for: %s', 'one-page' ), '' . get_search_query() . '' ); ?></h1> -->
+<!-- blog title ends -->
+<div class="blog_pages_wrapper default_bg search-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+          <!--page start-->
+  		<?php
+  		if ( have_posts() ) :
+  			while (have_posts()) {
+          the_post();
+          if (get_field('event_date')) {
+            $date = date('d.m.y', strtotime(get_field('event_date')));
+          } else {
+
+            $date = date('d.m.y', strtotime(get_the_date()));
+          }
+          $image;
+          if (!get_the_post_thumbnail_url()) {
+            $image = get_theme_file_uri('/assets/images/2018-Digital-Marketing.jpg');
+          } else {
+            $image = get_the_post_thumbnail_url();
+          }
+          ?>
+          <article class="post-item first-post">
+            <div class="post-thumbnail">
+              <div class="ratio-container">
+                <div class="ratio-content" style="background-image: url('<?php echo $image; ?>')"></div>
+              </div>
+              <a href="<?php the_permalink(); ?>" class="post-overlay"></a>
+            </div><!-- post-thumbnail -->
+            <div class="post-categories">
+              <?php
+              the_category();
+              ?>
+            </div><!-- post-categories -->
+            <a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a><!-- post-title -->
+            <div class="post-content">
+              <?php if (get_field('event_description')) { ?>
+                <p><?php echo wp_trim_words(get_field('event_description'), 55); ?></p>
+
+              <?php } else { ?>
+                <p><?php echo wp_trim_words(get_the_content(), 55); ?></p>
+              <?php } ?>
+            </div><!-- post-content -->
+
+            <div class="post-meta">
+              <a href="<?php echo the_permalink(); ?>" class="cta cta-read-more">Read more</a>
+              <div class="post-meta-content pull-right">
+                <a href="<?php echo get_author_posts_url($post->post_author); ?>" class="post-author"><img src="<?php echo get_theme_file_uri('/assets/images/Icons/icon-pen.svg'); ?>" alt="icon"><?php the_author(); ?></a>
+                <a class="post-date"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-calendar.svg'); ?>" alt="icon"><?php echo $date; ?></a>
+                <a href="<?php echo the_permalink(); ?>" class="post-comments has-comment"><img src="<?php echo get_theme_file_uri('assets/images/Icons/icon-comment0.svg'); ?>" alt="icon"><?php echo get_comments_number(); ?></a>
+              </div>
+            </div><!-- post-meta -->
+          </article><!-- article -->
+          <?php
+        }
+  		else :
+  			?>
+  		<article id="post-0" class="post no-results not-found">
+  			<header class="entry-header">
+  				<h1 class="entry-title">
+  					<?php _e( 'Nothing Found', 'one-page' ); ?>
+  				</h1>
+  			</header>
+  			<!-- .entry-header -->
+  			<div class="entry-content">
+  				<p>
+  					<?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'one-page' ); ?>
+  				</p>
+  			</div>
+  			<!-- .entry-content -->
+  		</article>
+    	<?php endif; ?>
+        <div class="pagination">
+          <div class="prev"><?php previous_posts_link('Previous posts', $postslist->max_num_pages); ?></div>
+          <div class="next"><?php next_posts_link('Next posts', $postslist->max_num_pages); ?></div>
+        </div>
+        <!--End Page-->
+      </div>
+      <!--Sidebar-->
+      <div class="col-md-4">
+        <?php dynamic_sidebar('primary-widget-area'); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="clear"></div>
+<?php
+get_footer();
+?>
